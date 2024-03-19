79f8e3e66c69b7dc5dfc88aeaf43b8ee2637704f
Elitsa Vrancheva <Elitsa@funda.nl>
2024-03-19 13:53:34 +0100

    Add headers to request

--- netlify/utils/openAi.ts
+++ netlify/utils/openAi.ts
@@ -8,6 +8,11 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_APIKEY || "",
 });
 
+const headers = {
+  "Access-Control-Allow-Origin": "*", // or specific origin
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
 async function handleQuestion(req: Request, res: Response) {
   const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: "user", content: req.query.q as string }],
@@ -16,7 +21,11 @@ async function handleQuestion(req: Request, res: Response) {
 
   const text = chatCompletion.choices[0].message.content;
 
-  res.json({ message: text });
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify(text),
+  };
 }
 
 export default handleQuestion;
